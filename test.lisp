d1f399637b5887ba604b22a78826d0bf85531cbb
hikettei <hikettei@gmail.com>
2021-08-19 00:42:14 +0900

    test code

--- test.lisp
+++ test.lisp
@@ -1,33 +1,35 @@
 
 (load "cl-cram.lisp")
 
-;(sb-profile:profile "CL-CRAM")
+(sb-profile:profile "CL-CRAM")
 (use-package :cl-cram)
 
 (defparameter x nil)
 (defparameter y nil)
 (defparameter z nil)
 
-;(init-progress-bar x "Loop1" 9000000)
-;(init-progress-bar y "Loop2" 9000000)
-;(init-progress-bar z "Loop3" 9000000)
+(init-progress-bar x "Loop1" 9000000)
+(init-progress-bar y "Loop2" 9000000)
+(init-progress-bar z "Loop3" 9000000)
 
 (fresh-line)
 
-;(dotimes (_ 900000)
-;  (update x 1)
-;  (update y 1)
-;  (update z 1))
+(dotimes (_ 900000)
+  (update x 1)
+  (update y 1)
+  (update z 1))
 
-;(fresh-line)
+(fresh-line)
 
-;(discard-all-progress-bar)
+(discard-all-progress-bar)
 
-;(sleep 1)
+(sleep 1)
+
 (with-progress-bar x "Loop1" 9000000
   (dotimes (i 9000000)
     (update x 1)))
 
 (fresh-line)
 
-;(sb-profile:report)
+(sb-profile:report)
+
